4aa59ed5b70a0ea246700999a3ae6d5c2bb42192
Dan Lyons <danielclyons@yahoo.com>
2020-07-29 22:11:25 -0400

    Sec 21.3.6 update

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
@@ -18,7 +19,14 @@ const server = new ApolloServer({
 
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
